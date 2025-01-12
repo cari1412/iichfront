b33e7f7d9460f12a27ed8a2758028811cd3ee69f
cari1412 <vadis1766@gmail.com>
2025-01-12 17:22:24 +0300

    aa

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',  // Включаем статическую генерацию
+  basePath: '/iichfront', // Замените на название вашего репозитория
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: '/iichfront/', // Замените на название вашего репозитория
+}
 
 export default nextConfig;
